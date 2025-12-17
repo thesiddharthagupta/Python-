be0f7866c06dc6d585ca4b3cdf626f99d6094453
thesiddharthagupta <thesiddgupta@gmail.com>
2025-12-18 02:28:43 +0530

    ODD_EVEN

---
+++ Basic_Odd_Even.py
@@ -0,0 +1,8 @@
+print("---------Here we are trying to find the ODD_EVEN numbers------")
+a = int(input("Enter a number: "))
+num = a
+
+if a % 2 == 0:
+    print(f"The number {num} is Even")
+else:
+    print(f"The number {num} is Odd")
